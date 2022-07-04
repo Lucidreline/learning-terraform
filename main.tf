44f0ede2c05d6a9507bc6a48c752afed64d5ae70
Lucidreline <casta.ma502@gmail.com>
2022-07-04 14:16:34 -0700

    Began creating clusters, created autoscaler.

--- main.tf
+++ main.tf
@@ -2,33 +2,49 @@ provider "aws" {
   region = "us-east-2"
 }
 
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnet_ids" "default" {
+  vpc_id = data.aws_vpc.default.id
+}
+
 variable "port" {
   description = "The port that the app is running on."
   default     = 8080
   type        = number
 }
 
-output "public_url" {
-  description = "Url used to reach the server."
-  value       = "http://${aws_instance.example-instance.public_ip}:${var.port}"
-}
+resource "aws_launch_configuration" "launch-config" {
+  image_id        = "ami-02f3416038bdb17fb"
+  instance_type   = "t2.micro"
+  security_groups = [aws_security_group.instance-security-group.id]
+  user_data       = <<-EOF
+                        #!/bin/bash
+                        echo "Hi there!" > index.html
+                        nohup busybox httpd -f -p ${var.port} &
+                        EOF
 
-resource "aws_instance" "example-instance" {
-  ami           = "ami-02f3416038bdb17fb"
-  instance_type = "t2.micro"
-
-  user_data = <<-EOF
-              #!/bin/bash
-              echo "Hi there!" > index.html
-              nohup busybox httpd -f -p ${var.port} &
-              EOF
-
-  vpc_security_group_ids = [aws_security_group.instance-security-group.id]
-
-  tags = {
-    "Name" = "First-Book-Example"
+  lifecycle {
+    create_before_destroy = true
   }
 }
+
+resource "aws_autoscaling_group" "book-autoscalerBoi" {
+  launch_configuration = aws_launch_configuration.launch-config.name
+  vpc_zone_identifier  = data.aws_subnet_ids.default.ids
+  min_size             = 2
+  max_size             = 5
+
+  tag {
+    key                 = "Name"
+    value               = "book-asg-example"
+    propagate_at_launch = true
+  }
+}
+
+
 
 resource "aws_security_group" "instance-security-group" {
   name = "book-example-security"
