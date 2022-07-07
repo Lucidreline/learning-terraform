2cd3da0301d06c8134d7ca01bbb5eb3138d28659
Lucidreline <casta.ma502@gmail.com>
2022-07-07 13:46:01 -0700

    Added Load balancer, can't connect to it yet.

--- main.tf
+++ main.tf
@@ -16,6 +16,13 @@ variable "port" {
   type        = number
 }
 
+output "alb_dns" {
+  value       = aws_lb.example.dns_name
+  description = "Load Balancer's domain name"
+}
+
+# ~ Security Groups ~
+
 resource "aws_security_group" "example" {
   # security groups for EC2's, Allows traffic from a certain port from any ip address
   name = "example-security"
@@ -28,11 +35,34 @@ resource "aws_security_group" "example" {
   }
 }
 
+resource "aws_security_group" "loadBalancer" {
+  name = "alb-security"
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+}
+
+# ~ Autoscaling Group ~
+
 resource "aws_autoscaling_group" "example" {
   # specifies the config for the cluster of EC2's
 
   launch_configuration = aws_launch_configuration.example.name
   vpc_zone_identifier  = data.aws_subnet_ids.default.ids
+  target_group_arns    = [aws_lb_target_group.target-group.arn]
+  health_check_type    = "ELB"
 
   min_size = 2
   max_size = 10
@@ -60,3 +90,63 @@ resource "aws_launch_configuration" "example" {
     create_before_destroy = true # we need this for autoscalling groups
   }
 }
+
+# ~ Load Balancer ~
+
+resource "aws_lb" "example" {
+  name               = "load-balancer"
+  load_balancer_type = "application"
+  subnets            = data.aws_subnet_ids.default.ids
+  security_groups    = [aws_security_group.loadBalancer.id]
+}
+
+resource "aws_lb_target_group" "target-group" {
+  name     = "example-target-group"
+  port     = var.port
+  protocol = "HTTP"
+  vpc_id   = data.aws_vpc.default.id
+
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200"
+    interval            = 15
+    timeout             = 3
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+}
+
+resource "aws_lb_listener_rule" "listener_rule" {
+  listener_arn = aws_lb_listener.example.arn
+  priority     = 100
+
+  condition {
+    path_pattern {
+      values = ["*"]
+    }
+  }
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.target-group.arn
+  }
+}
+
+resource "aws_lb_listener" "example" {
+  load_balancer_arn = aws_lb.example.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  # will return a 404 page if the request doesn't match one of the lister rules
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "404: Can't find that Page Buddy"
+      status_code  = 404
+    }
+  }
+}
+
