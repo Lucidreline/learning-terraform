c7d8972bbea2e9f5687c9aa3c0f57ee6578a356e
Lucidreline <casta.ma502@gmail.com>
2022-07-07 12:03:35 -0700

    Created a EC2 cluster.

--- main.tf
+++ main.tf
@@ -16,44 +16,9 @@ variable "port" {
   type        = number
 }
 
-resource "aws_launch_configuration" "launch-config" {
-  image_id        = "ami-02f3416038bdb17fb"
-  instance_type   = "t2.micro"
-  security_groups = [aws_security_group.instance-security-group.id]
-  user_data       = <<-EOF
-                        #!/bin/bash
-                        echo "Hi there!" > index.html
-                        nohup busybox httpd -f -p ${var.port} &
-                        EOF
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
-resource "aws_autoscaling_group" "book-autoscalerBoi" {
-  launch_configuration = aws_launch_configuration.launch-config.name
-  vpc_zone_identifier  = data.aws_subnet_ids.default.ids
-  min_size             = 2
-  max_size             = 5
-
-  tag {
-    key                 = "Name"
-    value               = "book-asg-example"
-    propagate_at_launch = true
-  }
-}
-
-resource "aws_lb" "book-loadBalancer" {
-  name               = "book-load-balancer-example-name"
-  load_balancer_type = "application"
-  subnets            = data.aws_subnet_ids.default.ids
-}
-
-
-
-resource "aws_security_group" "instance-security-group" {
-  name = "book-example-security"
+resource "aws_security_group" "example" {
+  # security groups for EC2's, Allows traffic from a certain port from any ip address
+  name = "example-security"
 
   ingress {
     from_port   = var.port
@@ -63,39 +28,35 @@ resource "aws_security_group" "instance-security-group" {
   }
 }
 
-resource "aws_lb_listener" "http" {
-  load_balancer_arn = aws_lb.book-loadBalancer.arn
-  port              = 80
-  protocol          = "HTTP"
+resource "aws_autoscaling_group" "example" {
+  # specifies the config for the cluster of EC2's
 
-  #return a default 404 page
-  default_action {
-    type = "fixed-response"
+  launch_configuration = aws_launch_configuration.example.name
+  vpc_zone_identifier  = data.aws_subnet_ids.default.ids
 
-    fixed_responce {
-      content_type = "text/plain"
-      message_body = "I can't find this boss"
-      status_code  = 404
-    }
+  min_size = 2
+  max_size = 10
+
+  tag {
+    key                 = "Name"
+    value               = "terraform-asg-example"
+    propagate_at_launch = true
   }
 }
 
-resource "aws_security_group" "alb" {
-  name = "loabBalancer-security"
+resource "aws_launch_configuration" "example" {
+  # specifies how to configure each EC2 instance in the auto scaling group
+  image_id        = "ami-02f3416038bdb17fb"
+  instance_type   = "t2.micro"
+  security_groups = [aws_security_group.example.id]
 
-  #let in http requests
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p ${var.port} &
+              EOF
 
-  # let out any request
-  egress = {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+  lifecycle {
+    create_before_destroy = true # we need this for autoscalling groups
   }
 }
